bcbcd7706010320f63754e41709631cde9fce061
Student Md Khushal warsi <khushalwarsi2402>
2026-03-24 02:31:41 +0530

    Updated Capacitor config and fixed webDir

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,10 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Bismillah Fresh Meats', 
-  webDir: 'www'
+  appId: 'com.bismillah.meats',
+  appName: 'Bismillah Fresh Meats',
+  // 👇 THIS IS THE MAGIC LINE WE CHANGED 👇
+  webDir: 'dist/sikandar-app/browser' 
 };
 
 export default config;
